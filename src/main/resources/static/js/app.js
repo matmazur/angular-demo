ce1f824876a16adb9ed451da38b7e1d5c5b61c8b
Bingo Bobo <matmazur90@gmail.com>
2019-02-02 17:28:35 +0100

    small changes to the calculator, added functions in controller

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,7 +1,25 @@
 var app = angular.module('app', []);
 
-app.controller('MainController', function(){
+app.controller('MainController', function () {
 
     this.exampleVariable = "This should be visible on the other side";
+
+    this.a=0;
+    this.b=0;
+
+    this.addResult = function(){
+       return this.a + this.b;
+    };
+    this.subResult = function(){
+        return this.a - this.b;
+    };
+
+    this.multiResult = function(){
+        return this.a * this.b;
+    };
+
+    this.divResult = function (){
+        return this.a / this.b;
+    };
 
 });
