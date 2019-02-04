0873391a401f5fac7c3756b5d0dedfeb7e7ac297
Bingo Bobo <matmazur90@gmail.com>
2019-02-04 16:52:37 +0100

    used directives ng-href,img,show,hide

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,93 +1,19 @@
 var app = angular.module('app', []);
 
-app.controller('CalculatorCtrl', function () {
+app.controller("MoviesCtrl", function(){
 
-    this.a = 0;
-    this.b = 0;
+    this.dummy = "delete this text to see the picture"
+    this.movies = ['JvqQY6b5IbA','MRwWucCZpLg'];
+    this.images =
+        ['http://images6.fanpop.com/image/photos/39400000/Everybody-Wants-Some-Logo-everybody-wants-some-39453890-500-281.jpg']
+});
 
-    this.addResult = function () {
-        return this.a + this.b;
-    };
-    this.subResult = function () {
-        return this.a - this.b;
-    };
 
-    this.multiResult = function () {
-        return this.a * this.b;
-    };
-
-    this.divResult = function () {
-        return this.a / this.b;
-    };
+app.controller("ShowHideCtrl", function(){
 
 });
 
-app.controller("HeaderController", function () {
 
-    this.hardText = "Welcome to the jungle";
-    this.newText = "";
-
-
-    this.toUpper = function () {
-        this.hardText = this.hardText.toUpperCase();
-    };
-
-    this.toLower = function () {
-        this.hardText = this.hardText.toLowerCase();
-    };
-
-    this.changeText = function () {
-
-        if (this.newText) {
-            this.hardText = this.newText;
-        }
-        if (this.newText === "") {
-            this.hardText = "Welcome to the jungle";
-        }
-    };
-});
-
-app.controller("BackController", function () {
-    this.changeBackground = function () {
-
-        var body = document.getElementsByTagName("body")[0];
-        var color = body.style.backgroundColor;
-        var colorToBe;
-
-        switch (color) {
-            case "red":
-                colorToBe = "blue";
-                break;
-            default:
-                colorToBe = "red";
-        }
-        body.style.backgroundColor = colorToBe;
-    };
-
-    this.changeBackgroundToWhite = function () {
-        var body = document.getElementsByTagName("body")[0];
-        body.style.backgroundColor = "white";
-    };
-});
-
-app.controller("ChangeController", function () {
-
-    this.status = "unchecked";
-    this.checkbox = true;
-
-    this.changeStatus = function () {
-
-        console.log(this.status);
-        console.log(this.checkbox);
-
-        if (this.checkbox) {
-            this.status = "checked";
-        } else {
-            this.status = "unchecked";
-        }
-    };
-
-});
 
 app.directive('ngEnter', function () {
     return function (scope, element, attrs) {
