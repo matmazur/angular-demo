b95c712dfc89bf46ed30064a134ac3daf269a06c
Bingo Bobo <matmazur90@gmail.com>
2019-02-05 14:27:57 +0100

    using ngStyle, ngClass

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,26 +1,42 @@
 var app = angular.module('app', []);
 
-app.controller("MoviesCtrl", function(){
+app.controller("MoviesCtrl", function () {
 
-    this.dummy = "delete this text to see the picture"
-    this.movies = ['JvqQY6b5IbA','MRwWucCZpLg'];
+    this.dummy = "delete this text to see the picture";
+    this.movies = ['JvqQY6b5IbA', 'MRwWucCZpLg'];
     this.images =
         ['http://images6.fanpop.com/image/photos/39400000/Everybody-Wants-Some-Logo-everybody-wants-some-39453890-500-281.jpg']
 });
 
 
-function Person(name,surname){
-    this.name=name;
-    this.surname=surname;
+function Person(name, surname) {
+    this.name = name;
+    this.surname = surname;
 }
 
-app.controller("RepeatCtrl", function(){
+app.controller("RepeatCtrl", function () {
 
     this.people = [
-        new Person("Jack","Black"),
-        new Person("Kelly","Kapoor"),
-        new Person("Martin","Scorsese")
-    ]
+        new Person("Jack", "Black"),
+        new Person("Kelly", "Kapoor"),
+        new Person("Martin", "Scorsese")
+    ];
+    this.odd = "odd";
+    this.even = "even";
+});
+
+
+app.controller("HeaderCtrl", function () {
+
+    this.changeStyle = function () {
+        this.changeHeader = {
+            'color': 'blue',
+            'font-size': '25px'
+        };
+
+    };
+
+
 });
 
 app.directive('ngEnter', function () {
