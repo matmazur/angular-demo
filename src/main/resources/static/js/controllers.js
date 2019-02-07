d64762097e9a1ab4c45720a598db429aec582830
Bingo Bobo <matmazur90@gmail.com>
2019-02-07 11:51:30 +0100

    added in home.html table that is using ProductCtrl that has field vm.products containing response data from REST API

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -1,2 +1,21 @@
 angular.module('app')
 
+
+    .controller("ProductCtrl", function ($http) {
+
+        var vm = this;
+
+
+        function refreshData() {
+            $http.get('/api/products')
+                .then(function success(response) {
+                        vm.products = response.data;
+                        console.log(vm.products);
+                    }, function error(response) {
+                        console.log('API failed with status ' + response.status);
+                    }
+                )
+        }
+
+        refreshData();
+    });
