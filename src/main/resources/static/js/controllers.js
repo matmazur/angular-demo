08ce26954cc520d3c8f9486920844c2a3e568190
Bingo Bobo <matmazur90@gmail.com>
2019-02-07 12:19:16 +0100

    added adding functionality in html file, upgraded addProduct function in ProductCtrl in case of empty fields in adding fomr

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -1,4 +1,5 @@
 angular.module('app')
+    .constant("appName", "restTestingApp")
 
 
     .controller("ProductCtrl", function ($http) {
@@ -17,16 +18,32 @@ angular.module('app')
                 )
         }
 
-        function addData(product) {
-            $http.post('/api/products', product)
-                .then(function success(response) {
-                        refreshData();
-                        vm.product = {};
-                    }, function error(response) {
-                        console.log('API failed with status ' + response.status);
-                    }
-                )
-        }
 
+        vm.product = {};
+        this.addProduct = function (product) {
+
+            if (product.name && product.producer && product.price) {
+                $http.post('/api/products', product)
+                    .then(function success(response) {
+                            refreshData();
+                            vm.product = {};
+                        }, function error(response) {
+                            console.log('API failed with status ' + response.status);
+                        }
+                    )
+            } else {
+                var errorText = "";
+                if (!product.name) {
+                    errorText += "fill name field\n";
+                }
+                if (!product.producer) {
+                    errorText += "fill name producer\n";
+                }
+                if (!product.price) {
+                    errorText += "fill name price\n";
+                }
+                alert(errorText);
+            }
+        };
         refreshData();
     });
